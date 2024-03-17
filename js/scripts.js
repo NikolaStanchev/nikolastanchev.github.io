2a7c6a7edb693b868d20fd7c50c021e0a8b5f6de
Nikola Stanchev <nikigt3@gmail.com>
2024-03-17 15:19:15 +0200

    add basic buttons and animation for them showing

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
-/*!
-* Start Bootstrap - Full Width Pics v5.0.6 (https://startbootstrap.com/template/full-width-pics)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-full-width-pics/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll(".hidden")
+
+hiddenElements.forEach((el) => observer.observe(el))
